1711aadf22f7f75c89c84df3a3a7e49450c658ba
MuthammalMahendiran <101463471+MuthammalMahendiran@users.noreply.github.com>
2022-04-21 23:05:40 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,41 @@
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB = 0x81;
+	DDRD =0x81;
+	// initialize port C
+	// motors connected across PC0...Pc3
+	while(1)
+	{
+		// FORWARD
+		PORTB = 0x01;
+		PORTD = 0x01;
+		_delay_ms(5000);
+		
+		// BACKWARD
+		PORTB = 0x80;
+		PORTD =0x80;
+		_delay_ms(5000);
+		
+		//RIGHT
+		PORTB =0x01;
+		PORTD =0x00;
+		_delay_ms(5000);
+		
+		//LEFT
+		PORTB =0x00;
+		PORTD =0x01;
+		_delay_ms(5000);
+		
+		//STOP
+		PORTB =0x00;
+		PORTD =0x00;
+		_delay_ms(3000);
+		
+	}
+	return 0;
+}
